2381554407575bc1ae5df257fd4398a40ce13dad
Stephanie Hyland <steph.hyland@gmail.com>
2014-03-03 13:43:53 -0500

    regex magic - identify which timepoint produced the smallest region

--- process_master.py
+++ process_master.py
@@ -2,7 +2,7 @@
 
 import sys
 import gzip
-
+import re
 
 datatype=sys.argv[1]
 base='/Users/stephanie/ll/results/'+datatype+'/'
@@ -34,6 +34,8 @@ for line in master:
     uniq_end=line.split()[2]
     # gonna look for the smallest region which contributed to the final region
     smallest = int(uniq_end)-int(uniq_start)
+    when_smallest=re.sub('_\d+[|]+[chr]*[1-9MXY]*[1-9]*','',line.split()[3])
+#    when_smallest=re.sub('[|]+[chr]+[1-9MXY][1-9]*','',line.split()[3])
     for time in times:
         if datatype+'_'+time+'_' in line:
             # yep, this region appeared at this timepoint... mark it down!
@@ -51,10 +53,11 @@ for line in master:
             size=int(end)-int(start)
             if size<smallest:
                 smallest = size
+                when_smallest=re.sub('_\d$','',segment.split()[3])
         else:
             time_pattern.append('0')
     # compared to the smallest hit in this region, how much has our final region expanded?
     final_size=int(uniq_end)-int(uniq_start)
     inflation=float(final_size)/smallest
-    uniq_line=chro+'\t'+uniq_start+'\t'+uniq_end+'\t'+uniq_ID+'\t'+'\t'.join(time_pattern)+'\t'+str(inflation)+'\n'
+    uniq_line=chro+'\t'+uniq_start+'\t'+uniq_end+'\t'+uniq_ID+'\t'+'\t'.join(time_pattern)+'\t'+str(inflation)+'\t'+when_smallest+'\n'
     new_master.write(uniq_line)
