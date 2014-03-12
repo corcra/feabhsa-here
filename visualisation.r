1862ec442c949e41a3553ff3b8aedcaa68d19962
Stephanie Hyland <steph.hyland@gmail.com>
2014-03-12 14:50:38 -0400

    keep all the pdfs in the pdf folder

--- visualisation.r
+++ visualisation.r
@@ -43,13 +43,13 @@ howmany_prefac<-rowSums(data[,time_points])
 howmany<-factor(howmany_prefac,c(1,2,3,4,5,6))
 data_howfreq<-data.frame(regions,howmany,first_appearance)
 ggplot(data_howfreq,aes(x=howmany,fill=regions))+geom_histogram(position="dodge")+scale_fill_manual(values=region_cols)+mytheme+ggtitle("How many timepoints do hits occur in?")+xlab("Number of timepoints")+facet_grid(~regions,margins=TRUE)
-ggsave("repetition_regions.pdf",width=10)
+ggsave("pdfs/repetition_regions.pdf",width=10)
 
 # For regions that are only seen once... when are they seen?
 cat("visualisation.r: Loner regions\n")
 data_loners<-data_howfreq[howmany_prefac==1,]
 ggplot(data_loners,aes(x=first_appearance,fill=regions))+geom_histogram(position="dodge")+scale_fill_manual(values=region_cols)+mytheme+xlab("Timepoint")+ggtitle("For regions appearing only once, when was it?")
-ggsave("loner_regions.pdf",width=10)
+ggsave("pdfs/loner_regions.pdf",width=10)
 
 # How many violations? (e.g. disappearance after appearance)
 #cat("visualisation.r: violations\n")
@@ -65,7 +65,7 @@ consistent<-viol_prefac==0
 consistent<-factor(consistent,c(TRUE,FALSE))
 data_consistent<-data.frame(regions,consistent,first_appearance)
 ggplot(data_consistent,aes(x=first_appearance,fill=consistent))+facet_grid(consistent~regions)+geom_histogram(position="dodge")+mytheme+scale_fill_manual(values=c("darkorchid1","grey60"))+ggtitle("When do consistent regions appear? (consistent meaning that once they appear, they do not disappear)")
-ggsave("consistent_regions.pdf",width=10)
+ggsave("pdfs/consistent_regions.pdf",width=10)
 
 # Now, are they earlier (<12.5) or later(>12.5)? (do this better!) (STRICTLY early v. STRICTLY late...)
 cat("visualisation.r: regions only appearing in early/late timepoints?\n")
@@ -76,7 +76,7 @@ when_restricted<-ifelse(all,"all",ifelse(early,"early only",ifelse(late,"late on
 when_restricted<-factor(when_restricted,c("all","early only","late only","mixed"))
 data_whenrestricted<-data.frame(regions,when_restricted)
 ggplot(data_whenrestricted,aes(x=when_restricted,fill=regions))+geom_histogram(position="dodge")+mytheme+scale_fill_manual(values=c(region_cols))+xlab("Only appears when...")+ggtitle("Many regions are only seen at early timepoints (WT,2min,5min)")
-ggsave("earlylate_regions.pdf",width=10)
+ggsave("pdfs/earlylate_regions.pdf",width=10)
 
 # Expand regions - this is a giant dataframe with duplicated entries whenever regions appear in multiple timepoints - the purpose is to be able to simultaneously plot not just new points but all points at a time, acces times via 'when'
 data_exp<-data.frame()
@@ -91,7 +91,7 @@ for (time in time_points){
 # When do the regions first appear? (note, they may disappear and reappear - this takes very first appearance)
 cat("visualisation.r: First appearance of regions!\n")
 ggplot(data,aes(x=first_appearance,fill=regions))+geom_bar(position="dodge")+scale_fill_manual(values=region_cols)+mytheme+facet_grid(~regions)
-ggsave("first_appearance.pdf",width=10)
+ggsave("pdfs/first_appearance.pdf",width=10)
 # What fraction of the regions appear for the first time at this timepoint?
 new_bool<-data_exp$when==data_exp$first_appearance
 new<-ifelse(new_bool,ifelse(data_exp$regions=="TSS", "new (TSS)", ifelse(data_exp$regions=="gene_body", "new (gene body)", "new (non gene)")),"old")
@@ -99,32 +99,32 @@ new<-factor(new,c("new (TSS)","new (gene body)","new (non gene)","old"))
 new_cols<-c(region_cols[1:3],"grey90")
 data_new<-cbind(data_exp,new)
 ggplot(data_new,aes(x=when,fill=new))+geom_bar(position="fill")+scale_fill_manual(values=new_cols)+mytheme+facet_grid(~regions)+ylab("Fraction of regions which are new at this timepoint")+xlab("Timepoint")+ggtitle("Does time after flavopiridol treatment aid discovery of intragenic regions?")
-ggsave("fraction_new.pdf",width=10)
+ggsave("pdfs/fraction_new.pdf",width=10)
 
 new<-ifelse(new_bool,ifelse(data_exp$regions=="TSS", "new (TSS)", ifelse(data_exp$regions=="gene_body", "new (gene body)", "new (non gene)")),ifelse(data_exp$regions=="TSS", "old (TSS)", ifelse(data_exp$regions=="gene_body", "old (gene body)","old (non gene)")))
 new<-factor(new,c("new (TSS)","old (TSS)","new (gene body)","old (gene body)", "new (non gene)", "old (non gene)"))
 data_new<-cbind(data_exp,new)
 new_cols<-c(region_cols[1],"firebrick4",region_cols[2],"orange3",region_cols[3],"turquoise4")
 ggplot(data_new,aes(x=when,fill=new))+geom_bar(position="dodge")+scale_fill_manual(values=new_cols)+mytheme+facet_grid(~regions)+ylab("Number of regions  at this timepoint")+xlab("Timepoint")
-ggsave("total_regions.pdf",width=10)
+ggsave("pdfs/total_regions.pdf",width=10)
 
 # Region sizes
 cat("visualisation.r: Region sizes!\n")
 sizes<-data$end-data$start
 size_dat<-data.frame(sizes,regions)
 ggplot(size_dat,aes(x=sizes,fill=regions))+geom_histogram(binwidth=50)+mytheme+xlim(0,2000)+scale_fill_manual(values=region_cols)+ggtitle("dREG Region sizes")+xlab("Size of dREG region (bp)")+ylab("Counts")+facet_grid(~regions,margins=FALSE,scales="free")
-ggsave("region_sizes.pdf",width=10)
+ggsave("pdfs/region_sizes.pdf",width=10)
 ggplot(size_dat,aes(x=sizes))+geom_histogram(binwidth=50,fill="gray")+mytheme+xlim(0,2000)+ggtitle("dREG Region sizes (all)")+xlab("Size of dREG region (bp)")+ylab("Counts")
-ggsave("all_sizes.pdf")
+ggsave("pdfs/all_sizes.pdf")
 
 # Inflation factor
 cat("visualisation.r: Inflation!\n")
 inflation<-data$inflation
 inf_dat<-data.frame(inflation,regions,first_appearance)
 ggplot(inf_dat,aes(x=inflation,fill=regions))+geom_histogram(binwidth=0.1)+mytheme+scale_fill_manual(values=region_cols)+ggtitle("Ratio by which smallest contributing region was embiggened")+xlab("Inflation factor")+ylab("Counts")+facet_grid(first_appearance~regions,margins=FALSE,scales="free")+xlim(0,7.5)
-ggsave("region_inflation.pdf",width=10)
+ggsave("pdfs/region_inflation.pdf",width=10)
 ggplot(inf_dat,aes(x=inflation))+geom_histogram(binwidth=0.5,fill="gray")+mytheme+ggtitle("Ratio by which smallest contributing region was embiggened (all)")+xlab("Inflation factor")+ylab("Counts")+xlim(0,20)
-ggsave("all_inflation.pdf")
+ggsave("pdfs/all_inflation.pdf")
 
 # Distance to closest TSS
 cat("visualisation.r: Distance to closest TSS!\n")
@@ -139,9 +139,9 @@ thresh_cols<-c("darkorchid1",region_cols[2],region_cols[3])
 dist_dat<-cbind(dist_dat,thresh)
 names(dist_dat)<-c("distance","regions","when","thresh")
 ggplot(dist_dat,aes(x=distance,fill=thresh))+geom_histogram(binwidth=250)+mytheme+scale_fill_manual(values=thresh_cols)+ggtitle("Distance to closest TSS (either direction)")+xlab("Distance (bp)")+ylab("Counts")+facet_grid(when~regions,margins=FALSE,scale="free",space="free")+xlim(0,25000)
-ggsave("region_distance.pdf",width=10)
+ggsave("pdfs/region_distance.pdf",width=10)
 ggplot(dist_dat,aes(x=distance))+geom_histogram(fill="gray",binwidth=500)+mytheme+ggtitle("Distance to closest TSS (either direction) (gene body and non-gene)")+xlab("Distance")+ylab("Counts")+xlim(0,25000)
-ggsave("all_distance.pdf",width=10)
+ggsave("pdfs/all_distance.pdf",width=10)
 
 # dREG scores
 #cat("make_histograms.r: dREG scores!\n")
@@ -158,7 +158,7 @@ gene_only_hits<-read.table('/Users/stephanie/ll/data/genes/lists/dREG_IDs_onlybo
 data_geneonly<-data.frame(regions,howmany,first_appearance)
 data_geneonly<-data_geneonly[data$"dREG_id"%in%gene_only_hits[,1],]
 ggplot(data_geneonly,aes(x=first_appearance,fill=regions))+geom_histogram(position="dodge")+scale_fill_manual(values=region_cols)+mytheme+ggtitle("When do the hits inside gene bodies (only) occur?")+xlab("Timepoint")+facet_grid(~regions,margins=TRUE)
-ggsave("gene_only_when.pdf",width=10)
+ggsave("pdfs/gene_only_when.pdf",width=10)
 
 #  Histone modifications! (have to make long form)
 counts_per_region<-function(start,stop,counts,norm){
@@ -184,5 +184,5 @@ mod_dat<-mod_dat[is.finite(mod_dat$long_counts),]
 names(mod_dat)<-c("dREG_id","mods","counts","regions","first_appearance")
 print(mod_dat[which(mod_dat$counts==max(mod_dat[mod_dat$mods=="H3K4me1",]$counts)),]$dREG_id)
 ggplot(mod_dat,aes(x=mods,y=counts,colour=mods,group=mods))+geom_boxplot(notch=TRUE)+facet_grid(~.regions)+scale_y_log10()+mytheme+ylab("Histone marks per base (per million read) : log10")+xlab("Marks and genomic regions")
-ggsave("histone_regions.pdf")
+ggsave("pdfs/histone_regions.pdf")
 
