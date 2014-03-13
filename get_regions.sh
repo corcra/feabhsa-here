275a5cbc815779daf22b41f72deec855cf8544bb
Stephanie Hyland <steph.hyland@gmail.com>
2014-03-13 15:54:01 -0400

    including the strand of the closest gene

--- get_regions.sh
+++ get_regions.sh
@@ -10,7 +10,7 @@ gene_list=$data/genes/gene_list.bed
 TSS=$data/genes/TSS.bed
 gene_body=$data/genes/gene_body.bed
 non_gene=$data/genes/non_genes.bed
-awk 'BEGIN{OFS="\t"}{ if ($6=="+") { print $1, $2, $2+1, $4 } else { print $1, $3-1, $3, $4 } }' $gene_list > gene_starts.temp
+awk 'BEGIN{OFS="\t"}{ if ($6=="+") { print $1, $2, $2+1, $4, $6 } else { print $1, $3-1, $3, $4, $6 } }' $gene_list > gene_starts.temp
 
 # Markers (note using bedGraphs)
 H3K4me1=$data/ChIPseq/bedgraphs/H3K4me1_Meiss.sorted.bedGraph
@@ -30,8 +30,7 @@ bedmap --fraction-ref 0.5 --indicator pred.temp $non_gene > non_gene.temp
 
 # Get the distance to the nearest TSS... (for all hits!) (modify the master)
 echo "Getting distance to closest gene start."
-bedtools closest -d -a pred.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-1), $NF }' > dist.pre.temp
-#bedtools closest -D "a" -a pred.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-1), $NF }' > dist.pre.temp
+bedtools closest -d -a pred.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-2), $NF, $(NF-1) }' > dist.pre.temp
 python post_bedtools_join_closest.py
 # (creates dist.temp)
 
@@ -43,8 +42,11 @@ bedmap --delim '\t' --range 500 --sum pred.temp $H3K27ac > H3k27ac.temp
 
 echo "Combining!"
 paste pred.temp TSS.temp gb.temp non_gene.temp dist.temp H3K4me1.temp H3K4me3.temp H3K27ac.temp > comb.temp
-gunzip -c $predfile | head -1 | awk '{ print $0, "TSS","gene_body","non_gene","closest_gene","distance","H3K4me1","H3K4me3","H3K27ac" }' > headerfile.temp
+gunzip -c $predfile | head -1 | awk '{ print $0, "TSS","gene_body","non_gene","closest_gene","distance","strand","H3K4me1","H3K4me3","H3K27ac" }' > headerfile.temp
 cat headerfile.temp comb.temp | gzip -c > ${predfile/_uniq/_marked}
 
 echo "Tidying up!"
 #rm -v *.temp
+
+# misc code fragments
+#bedtools closest -D "a" -a pred.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-1), $NF }' > dist.pre.temp
