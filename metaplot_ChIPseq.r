9a18e0110a5e73cda1797b98cf91c98db5880f5f
Stephanie Hyland <steph.hyland@gmail.com>
2014-03-24 15:50:36 -0400

    fixed the preprocessed section

--- metaplot_ChIPseq.r
+++ metaplot_ChIPseq.r
@@ -11,9 +11,10 @@ get_metaplot<-function(bedfile,bw,hw,stepp,region_name){
     res<-meta.subsample(bedfile,bw,NULL,halfWindow=hw,step=stepp)
     lv<-colMeans(res[[1]])
     uq<-res[[2]]
-    lq<-res[[2]]
+    lq<-res[[3]]
     d<-seq(-length(lv)/2,length(lv)/2-1)
     region<-rep(region_name,length(lv))
+    region<-factor(region_name,c("gene_start","gene_body","non_gene"))
     dl<-data.frame(d,lv,uq,lq,region_name)
     return(dl)
 }
@@ -24,25 +25,27 @@ plot_metaplot<-function(dl,dataname){
     region_cols<-c("firebrick1","orange1","turquoise3","gray")
 
     browser()
-    ggplot(dl,aes(x=d,y=lv,colour=region_name,group=region_name))+geom_point(cex=0.5,show_guide=FALSE)+facet_grid(~region_name)+mytheme+xlab("Distance to centre (bp)")+ylab("Average ChIP-seq signal")+scale_colour_manual(values=region_name_cols)+geom_line(y=0,linetype="dashed",show_guide=FALSE,colour="black")+ggtitle(paste0("Metagene plot (",dataname,")"))+geom_ribbon(aes(ymin=lq,ymax=uq,alpha=0.5))
+    ggplot(dl,aes(x=d,y=lv,colour=region_name,group=region_name))+geom_point(cex=0.5,show_guide=FALSE)+facet_grid(~region_name)+mytheme+xlab("Distance to centre (bp)")+ylab("Average ChIP-seq signal")+scale_colour_manual(values=region_cols)+ggtitle(paste0("Metagene plot (",dataname,")"))+geom_ribbon(aes(ymin=lq,ymax=uq,alpha=0.5),show_guide=FALSE)
     ggsave(paste0(dataname,".pdf"))
 }
 
 if(!preprocess){
-    # this needs more tweaking
-    basepath<-'/Users/stephanie/ll/data/ChIPseq/bw/'
-    gs<-read.table(paste0(basepath,name,'_genestart.txt'),header=TRUE)
-    gb<-read.table(paste0(basepath,name,'_genebody.txt'),header=TRUE)
-    ng<-read.table(paste0(basepath,name,'_nongene.txt'),header=TRUE)
+    
+    H3K4me1_data<-read.table(file="H3K4me1_metaplot.txt",header=TRUE,as.is=TRUE)
+    H3K4me1_data$region_name<-factor(H3K4me1_data$region_name,c("gene_start","gene_body","non_gene"))
+    plot_metaplot(H3K4me1_data,"H3K4me1")
 
-    region<-rep("gene_start",nrow(gs))
-    gs<-cbind(gs,region)
-    region<-rep("gene_body",nrow(gb))
-    gb<-cbind(gb,region)
-    region<-rep("non_gene",nrow(ng))
-    ng<-cbind(ng,region)
-    dl<-rbind(gs,gb,ng)
+    H3K4me3_data<-read.table(file="H3K4me3_metaplot.txt",header=TRUE,as.is=TRUE)
+    H3K4me3_data$region_name<-factor(H3K4me3_data$region_name,c("gene_start","gene_body","non_gene"))
+    plot_metaplot(H3K4me3_data,"H3K4me3")
 
+    H3K27ac_data<-read.table(file="H3K27ac_metaplot.txt",header=TRUE,as.is=TRUE)
+    H3K27ac_data$region_name<-factor(H3K27ac_data$region_name,c("gene_start","gene_body","non_gene"))
+    plot_metaplot(H3K27ac_data,"H3K27ac")
+
+    H3_data<-read.table(file="H3_metaplot.txt",header=TRUE,as.is=TRUE)
+    H3_data$region_name<-factor(H3_data$region_name,c("gene_start","gene_body","non_gene"))
+    plot_metaplot(H3_data,"H3")
 } else{
     gene_start<-read.table('/Users/stephanie/ll/results/FP/dREG_gs.bed')
     gene_body<-read.table('/Users/stephanie/ll/results/FP/dREG_gb.bed')
@@ -54,8 +57,8 @@ if(!preprocess){
     gb_dat<-get_metaplot(gene_start,H3K4me1,hw,stepp,"gene_body")
     ng_dat<-get_metaplot(gene_start,H3K4me1,hw,stepp,"non_gene")
     H3K4me1_data<-rbind(gs_dat,gb_dat,ng_dat)
+    write.table(H3K4me1_data,row.names=F,col.names=T,quote=F,file="H3K4me1_metaplot.txt")
     plot_metaplot(H3K4me1_data,"H3K4me1")
-    browser()
 
     cat("H3K4me3!\n")
     H3K4me3<-load.bigWig('/Users/stephanie/ll/data/ChIPseq/bw/H3K4me3_Marson.sorted.bw')
@@ -63,6 +66,7 @@ if(!preprocess){
     gb_dat<-get_metaplot(gene_start,H3K4me3,hw,stepp,"gene_body")
     ng_dat<-get_metaplot(gene_start,H3K4me3,hw,stepp,"non_gene")
     H3K4me3_data<-rbind(gs_dat,gb_dat,ng_dat)
+    write.table(H3K4me3_data,row.names=F,col.names=T,quote=F,file="H3K4me3_metaplot.txt")
 
     cat("H3K27ac!\n")
     H3K427ac<-load.bigWig('/Users/stephanie/ll/data/ChIPseq/bw/H3K27Ac.sorted.bw')
@@ -70,6 +74,7 @@ if(!preprocess){
     gb_dat<-get_metaplot(gene_start,H3K27ac,hw,stepp,"gene_body")
     ng_dat<-get_metaplot(gene_start,H3K27ac,hw,stepp,"non_gene")
     H3K27ac_data<-rbind(gs_dat,gb_dat,ng_dat)
+    write.table(H3K27ac_data,row.names=F,col.names=T,quote=F,file="H3K27ac_metaplot.txt")
 
     cat("H3!\n")
     H3<-load.bigWig('/Users/stephanie/ll/data/ChIPseq/bw/H3_align.sorted.bw')
@@ -77,5 +82,6 @@ if(!preprocess){
     gb_dat<-get_metaplot(gene_start,H3,hw,stepp,"gene_body")
     ng_dat<-get_metaplot(gene_start,H3,hw,stepp,"non_gene")
     H3_data<-rbind(gs_dat,gb_dat,ng_dat)
+    write.table(H3_data,row.names=F,col.names=T,quote=F,file="H3_metaplot.txt")
 }
 
