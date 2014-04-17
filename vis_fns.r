a997de9681b5a8b29c73b574b5fc27495caebec3
Stephanie Hyland <steph.hyland@gmail.com>
2014-04-17 14:45:44 -0400

    wasn't calculating violations correctly

--- vis_fns.r
+++ vis_fns.r
@@ -1,10 +1,11 @@
 get_violations<-function(df){
     n<-nrow(df)
     violations<-rep(0,n)
-    one_seen<-rep(FALSE,n)
+    one_seen<-(df[,1]==1)
     for (col in seq(2,ncol(df))){
         one_seen<-one_seen|df[,col]==1
         violations<-violations+1*((one_seen)*(df[,col]==0))
+        browser()
     }
     return(violations)
 }
