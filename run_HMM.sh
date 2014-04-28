a851704095eef8abf60113dda0912f4c80d2b011
Stephanie Hyland <steph.hyland@gmail.com>
2014-04-27 22:07:50 -0400

    after running the hmm, fetches the # of hits in promoter/gb regions for each gene..

--- run_HMM.sh
+++ run_HMM.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+suffix=40m_SVM      # eg full_SVM, 40m_SVM...
+
 hmm_dir=/Users/stephanie/ll/from_hojoong
 cd $hmm_dir
 
 bg_dir=/Users/stephanie/ll/data/FP/bedgraphs
 time=$1
 binsize=$2
+
+
 #thre=$((2*$binsize))
 thre=25000
 #if [ $thre -lt $((2*$binsize)) ]
@@ -22,24 +26,24 @@ bi_minus=$hmm_dir/bi/V6.5_$time\min.m.bi
 bi0_plus=$hmm_dir/bi/V6.5_0min.p.bi
 bi0_minus=$hmm_dir/bi/V6.5_0min.m.bi
 
-genes_preformatted=/Users/stephanie/ll/data/genes/analyse_FP/gene_list_with_overlaps.bed
+genes_preformatted=/Users/stephanie/ll/results/$suffix/genes_gs_hit.bed
 genelist=$hmm_dir/list/genes_over$thre.txt
 
 result=$hmm_dir/result/V6.5_$time\min_bin$binsize.txt
 
-# prepare the genelist
+# --- prepare the genelist --- #
 if ! [ -a $genelist ]
     then
         echo "Processing genelist!"
         awk 'BEGIN{OFS="\t"}{ if ((($3-$2)>'$thre')&&($7>0)) print NR,$4,$4,$1,$6,$2,$3 }' $genes_preformatted > $genelist
+        echo "There are" `wc -l $genelist | awk '{print $1 }'` "genes remaining!"
 fi
 
-echo "There are" `wc -l $genelist | awk '{print $1 }'` "genes remaining!"
 
-# prepare the bedgraphs
-if ! [ -a $bi_plus ]
+#  --- prepare the bedgraphs --- #
+if ! [ -e $bi_plus ]
     then
-        if ! [ -a $bg_plus.gz ]
+        if [ -e $bg_plus.gz ] && ! [ -e $bg_plus ]
             then
                 echo "Unzipping bedgraph ("$bg_plus")"
                 gunzip $bg_plus.gz
@@ -48,9 +52,9 @@ if ! [ -a $bi_plus ]
         ./code/prereadbg -i $bg_plus -o $bi_plus -c list/mm9chr.txt
 fi
 
-if ! [ -a $bi_minus ]
+if ! [ -e $bi_minus ]
     then
-        if ! [ -a $bg_minus.gz ]
+        if [ -e $bg_minus.gz ] && ! [ -e $bg_minus ]
             then
                 echo "Unzipping bedgraph ("$bg_minus")"
                 gunzip $bg_minus.gz
@@ -59,5 +63,18 @@ if ! [ -a $bi_minus ]
         ./code/prereadbg -i $bg_minus -o $bi_minus -c list/mm9chr.txt
 fi
 
-# run the HMM
+# --- run the HMM --- #
 ./code/hmm2 -o $result -p $bi_plus -m $bi_minus -p0 $bi0_plus -m0 $bi0_minus -b $binsize -g $genelist -c list/mm9chr.txt
+
+# --- check for gene-body enhancers --- #
+# Have a file of hits specifically in the gene body...
+echo `sed '1q;d' $result` "chr" "start" "end" "n_gs" "n_gb" > header.temp
+# sort the genelist
+sort -k4,4 $genes_preformatted > genes.temp
+# sort the result-list, then perform a join on the genename field... the result should be the addition of two columns - first is the number of dREG hits in the gene start region, second is number of hits in the gene body ... we can use this for later analysis!
+sort -k2,2 $result | join -1 2 -2 4 - genes.temp | awk 'BEGIN{OFS="\t"}{ print $2, $1, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 }' > $result.temp
+cat header.temp $result.temp > $result.final
+mv $result.final $result
+
+# --- Tidy! --- #
+rm -v *.temp
