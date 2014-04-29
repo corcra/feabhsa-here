b7b8648321f8d1d5c072823e2ae2bf7832904edd
Stephanie Hyland <steph.hyland@gmail.com>
2014-04-29 18:52:21 -0400

    restrict to consistent regions

---
+++ pipeline/QC.r
@@ -0,0 +1,40 @@
+# Perform quality control (of arbitrary stlye)
+
+# Options
+consistent<-TRUE
+
+# Data?
+suffix<-"40m_SVM"           # 40m_SVM or full_SVM
+#suffix<-"full_SVM"           # 40m_SVM or full_SVM
+
+# Paths
+inpath<-paste0('/Users/stephanie/ll/results/',suffix,'/FP_dREG_regions_marked.bed.gz')
+outpath<-paste0('/Users/stephanie/ll/results/',suffix,'/FP_dREG_regions_filtered.bed')
+
+# Set up some vars
+time_points<-c("X0","X2","X5","X12.5","X25","X50")
+
+# Functions and stuff
+get_violations<-function(df){
+    n<-nrow(df)
+    violations<-rep(0,n)
+    one_seen<-(df[,1]==1)
+    for (col in seq(2,ncol(df))){
+        one_seen<-one_seen|df[,col]==1
+        violations<-violations+1*((one_seen)*(df[,col]==0))
+    }
+    return(violations)
+}
+
+# Get the data!
+data<-read.table(inpath,header=T,na.strings="NAN")
+
+# Apply filters, etc.
+if (consistent){
+    print("Restricting to consistent hits!")
+    violations<-get_violations(data[,time_points])
+    consistent<-violations==0
+    data_cons<-subset(data,consistent==TRUE)
+}
+
+write.table(data_cons,file=outpath,quote=F,row.names=F,sep="\t")
