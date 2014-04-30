2d628abb81e964dd7ed65ff8d4a32cb68b7f07ff
Stephanie Hyland <steph.hyland@gmail.com>
2014-04-30 16:44:46 -0400

    stripped down, clearer, also uses low confidence regions

--- pipeline/gene_focused.sh
+++ pipeline/gene_focused.sh
@@ -3,19 +3,67 @@
 suffix=40m_SVM      # eg 40m_SVM, full_SVM
 #suffix=full_SVM      # eg 40m_SVM, full_SVM
 
+THRE_high=0.8
+THRE_low=0.5
+
 genefolder=/Users/stephanie/ll/data/genes
 
 results=/Users/stephanie/ll/results/$suffix
-confident=$results/FP_dREG_confident_0.8.bed.gz
-uncertain=$results/FP_dREG_uncertain_0.5.bed.gz
+confident=$results/dREG_regions_confident_$THRE_high.bed.gz
+maybe=$results/dREG_regions_maybe_$THRE_low.bed.gz
+
 genelist=$genefolder/gene_list.bed
 
-# --- produce the goodlist --- #
-gunzip -c $confident | sed '1d' | bedmap --range 500 --multidelim "|" --delim "|" --echo --count --echo-map $genelist - > $results/gene_list_with_overlaps.bed
-python gene_focused_tidy.py $results/gene_list_with_overlaps.bed $results/gene_list_with_overlaps_tidy.bed
-mv $results/gene_list_with_overlaps_tidy.bed $results/gene_list_with_overlaps.bed
+# --- get the gene-dREG overlap for both confidence datasets... --- #
+gunzip -c $maybe | sed '1d' | bedmap --range 500 --multidelim "|" --delim "|" --echo --count --echo-map $genelist - > $results/genes_maybe_overlap.bed
+python gene_focused_tidy.py $results/genes_maybe_overlap.bed $results/genes_maybe_overlap_tidy.bed
+mv $results/genes_maybe_overlap_tidy.bed $results/genes_maybe_overlap.bed
+
+gunzip -c $confident | sed '1d' | bedmap --range 500 --multidelim "|" --delim "|" --echo --count --echo-map $genelist - > $results/genes_confident_overlap.bed
+python gene_focused_tidy.py $results/genes_confident_overlap.bed $results/genes_confident_overlap_tidy.bed
+mv $results/genes_confident_overlap_tidy.bed $results/genes_confident_overlap.bed
+
+# --- produce a list of genes with hits in their TSS - based on the maybe data --- #
+awk '{ if ($8!=0) print $0 }' $results/genes_maybe_overlap.bed > $results/genes_gs_hit.bed
+
+# --- produce a list of genes with hits in their gene body - based on the confident data --- #
+awk '{ if ($9!=0) print $0 }' $results/genes_confident_overlap.bed | bedmap --skip-unmapped --echo - $results/genes_gs_hit.bed > $results/genes_gs_gb_hit.bed
+
+# --- produce a list of genes with NO HITS in their gene body - based on the maybe data --- #
+awk '{ if ($9==0) print $0 }' $results/genes_gs_hit.bed > $results/genes_gs_nogb_hit.bed
+
+echo "# genes with dREG hit at TSS:" `wc -l $results/genes_gs_hit.bed | awk '{ print $1 }'`
+# now, which of these have hits in the body?
+echo "# genes with dREG hit at TSS and in gene body:" `wc -l $results/genes_gs_gb_hit.bed | awk '{ print $1 }'`
+
+# --- create a list for running the HMM... --- #
+cat $results/genes_gs_gb_hit.bed $results/genes_gs_nogb_hit.bed > $results/genes_for_analysis.bed
+
+# --- clean up... don't really need these after the master genelist has been created... --- #
+#rm -v genes_gs_hit.bed
+#rm -v genes_gs_gb_hit.bed
+#rm -v genes_gs_nogb_hit.bed
+
+## --- produce the goodlist --- #
+#gunzip -c $confident | sed '1d' | bedmap --range 500 --multidelim "|" --delim "|" --echo --count --echo-map $genelist - > $results/gene_list_with_overlaps.bed
+#python gene_focused_tidy.py $results/gene_list_with_overlaps.bed $results/gene_list_with_overlaps_tidy.bed
+#mv $results/gene_list_with_overlaps_tidy.bed $results/gene_list_with_overlaps.bed
 # --- produce the bad list --- #
-#gunzip -c $uncertain | sed '1d' | bedops --not-element-of -1
+#gunzip -c $maybe | sed '1d' | bedops --not-element-of -1
+#
+
+# get the genes with hits at gene_start
+#awk '{ if ($7==0) print $0 }' $results/gene_list_with_overlaps.bed > $results/genes_no_hits.bed
+
+
+# not sure how useful thi sstuff is #
+# get the genes with no hits at gene_start
+#awk '{ if (($7>0)&&($8==0)) print $0 }' $results/gene_list_with_overlaps.bed > $results/genes_only_body_hits.bed
+#awk '{ print $10 }' $results/genes_only_body_hits.bed | tr ';' '\n' > $results/dREG_IDs_onlybody.txt
+#echo -e 'ID\tstart\tbody' > $results/gene_IDs_bodyhit.txt
+#awk 'BEGIN{OFS="\t"}{ if ($9>0) print $4,$8,$9 }' $results/gene_list_with_overlaps.bed >> $results/gene_IDs_bodyhit.txt
+# (use this in the visualisation script! when are these guys discovered?)
+
 
 # R code
 # library(ggplot2)
@@ -27,20 +75,3 @@ mv $results/gene_list_with_overlaps_tidy.bed $results/gene_list_with_overlaps.be
 # ggsave("pdfs/hits_inpromoter.pdf")
 # ggplot(dat,aes(x=body))+geom_histogram(binwidth=0.5)+theme_bw()+ggtitle("Number of dREG hits in gene bodies.")
 # ggsave("pdfs/hits_inbody.pdf")
-#
-
-# get the genes with hits at gene_start
-awk '{ if ($7!=0) print $0 }' $results/gene_list_with_overlaps.bed > $results/genes_gs_hit.bed
-echo "# genes with dREG hit at TSS:" `wc -l $results/genes_gs_hit.bed | awk '{ print $1 }'`
-# now, which of these have hits in the body?
-echo "# genes with dREG hit at TSS and in gene body:" `awk '{ if ($8!=0) print $0 }' $results/genes_gs_hit.bed | wc -l`
-
-
-# not sure how useful thi sstuff is #
-# get the genes with no hits at gene_start
-awk '{ if ($7==0) print $0 }' $results/gene_list_with_overlaps.bed > $results/genes_no_hits.bed
-awk '{ if (($7>0)&&($8==0)) print $0 }' $results/gene_list_with_overlaps.bed > $results/genes_only_body_hits.bed
-awk '{ print $10 }' $results/genes_only_body_hits.bed | tr ';' '\n' > $results/dREG_IDs_onlybody.txt
-echo -e 'ID\tstart\tbody' > $results/gene_IDs_bodyhit.txt
-awk 'BEGIN{OFS="\t"}{ if ($9>0) print $4,$8,$9 }' $results/gene_list_with_overlaps.bed >> $results/gene_IDs_bodyhit.txt
-# (use this in the visualisation script! when are these guys discovered?)
