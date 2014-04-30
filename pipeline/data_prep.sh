e7728f742103cf1982dc21bbb89220cad6aaaf4c
Stephanie Hyland <steph.hyland@gmail.com>
2014-04-30 15:58:46 -0400

    big changes - now we have a high confidence and low confidence set... do identical analysis on each, except for threshold and additioanl QC on the high-confidence

--- pipeline/data_prep.sh
+++ pipeline/data_prep.sh
@@ -2,26 +2,33 @@
 # author:       Stephanie Hyland (sh985@cornell.edu)
 # date:         February-April 2014
 # description:  This is something of a 'master' pipeline. It does the following:
-#               1. Combine dREG predictions (above thresh) from all timepoints (into $rawlist).
+#               1. Combine dREG predictions (above thresh) from all timepoints (into $raw_high).
 #               2. Format this nicely (via process_master.py)
 #               3. Classify all points by genomic region, get overlap with histone marks.
 
-# Threshold
-THRE=0.8
+# Threshold ... obviously, select these carefully
+THRE_high=0.8
+THRE_low=0.5
 
 # Inputs!
 datatype=FP     # eg FP, TRP... not sure why I'm still asking this
-suffix=full_SVM         # eg full_SVM, 40m_SVM... other?
+suffix=40m_SVM  # eg full_SVM, 40m_SVM... other?
 
 # Paths
 data=/Users/stephanie/ll/data
 folder=$data/dREG/$suffix
 results=/Users/stephanie/ll/results/$suffix
 
-# Main files
-rawlist=$results/$datatype\_dREG_regions.bed.gz
-regionlist=$results/$datatype\_dREG_regions_uniq.bed.gz
-final=$results/$datatype\_dREG_regions_marked.bed.gz
+# Main files (note - no QC on the maybe hits! only use this to exclude things, really...)
+# --- confident ---
+raw_high=$results/dREG_regions_$THRE_high.bed.gz
+proc_high=$results/dREG_regions_proc_$THRE_high.bed.gz
+pre_QC_confident=$results/dREG_regions_preQC_$THRE_high.bed.gz
+confident=$results/$datatype\_dREG_regions_confident_$THRE_high.bed.gz
+# --- uncertain ---
+raw_low=$results/dREG_maybe_$THRE_low.bed.gz
+proc_low=$results/dREG_maybe_proc_$THRE_low.bed.gz
+maybe=$results/dREG_regions_maybe_$THRE_low.bed.gz
 
 # Genes (for later)
 gene_list=$data/genes/gene_list.bed
@@ -39,83 +46,143 @@ do
     echo "Encorporating" $datatime
     # Set up vars
     preds=$folder/$datatype\_$datatime.predictions.bedGraph.gz
-    pos=$results/$datatype\_$datatime.positive.bedGraph.gz
+    gunzip -c $preds > $preds.temp
+    high_pos=$results/$datatype\_$datatime.pos.$THRE_high.bedGraph.gz
+    low_pos=$results/$datatype\_$datatime.pos.$THRE_low.bedGraph.gz
 
     # What is the score distribution? (can plot this later as necessary)
     echo "Getting scores."
-    gunzip -c $preds > $preds.temp
     bedmap --fraction-ref 0.5 --echo --skip-unmapped $preds.temp $gene_start | gzip -c > ${preds/.predictions/.gs.predictions}
     bedmap --fraction-ref 0.5 --echo --skip-unmapped $preds.temp $gene_body | gzip -c > ${preds/.predictions/.gb.predictions}
     bedmap --fraction-ref 0.5 --echo --skip-unmapped $preds.temp $non_gene | gzip -c > ${preds/.predictions/.ng.predictions}
 
     # Expands positive regions into window +-50, merges these.
-    echo "Getting regions."
-    awk 'BEGIN{OFS="\t"} ($4 > '"$THRE"') { print $1,$2-50,$3+51,$4 }' $preds.temp | sort-bed - | bedops --merge - | awk 'BEGIN{OFS="\t"}{ print $1, $2, $3, "'$datatype'_" "'${datatime/min/}'_" NR }' > $pos.temp
+    echo "Getting high-confidence regions."
+    awk 'BEGIN{OFS="\t"} ($4 > '"$THRE_high"') { print $1,$2-50,$3+51,$4 }' $preds.temp | sort-bed - | bedops --merge - | awk 'BEGIN{OFS="\t"}{ print $1, $2, $3, "'$datatype'_" "'${datatime/min/}'_" NR }' > $high_pos.temp
+    echo "Getting low-confidence regions."
+    awk 'BEGIN{OFS="\t"} ($4 > '"$THRE_low"') { print $1,$2-50,$3+51,$4 }' $preds.temp | sort-bed - | bedops --merge - | awk 'BEGIN{OFS="\t"}{ print $1, $2, $3, "'$datatype'_" "'${datatime/min/}'_" NR }' > $low_pos.temp
 
+    # --- high-confidence --- #
     # Create the master list, if necessary
-    if ! [ -a $rawlist ]
+    if ! [ -a $raw_high ]
     then
-        echo "Creating master list!"
-        gzip -c $pos.temp > $rawlist
+        echo "Creating master list (high conf)!"
+        gzip -c $high_pos.temp > $raw_high
     fi
-
-    # Checks if this data already exists in the master list
-    if gunzip -c $rawlist | grep -q $datatype\_${datatime/min/}_
+    
+    # Checks if this data already exists in the master list (high)
+    if gunzip -c $raw_high | grep -q $datatype\_${datatime/min/}_
     then
-        echo "Master list already includes these regions - skipping."
+        echo "Master list (high-conf) already includes these regions - skipping."
     else
         # Adds regions to existing list (assume same if ANY overlap between sets)
         echo "Updating master list."
-        gunzip -c $rawlist | bedmap --delim '|' --multidelim '|' --echo --indicator $pos.temp - > new_or_old.temp
+        gunzip -c $raw_high | bedmap --delim '|' --multidelim '|' --echo --indicator $high_pos.temp - > new_or_old.temp
         awk 'BEGIN{FS="|"}{ if ($2==0) print $1 }' new_or_old.temp > new.temp
         awk 'BEGIN{FS="|"}{ if ($2==1) print $1 }' new_or_old.temp > old.temp
-        gunzip -c $rawlist | bedmap --delim '|' --multidelim '|' --echo --echo-map - old.temp  > rawlist.temp
-        cat rawlist.temp new.temp | sort-bed - > rawlist_updated.temp
-        gzip -c rawlist_updated.temp > $rawlist
+        gunzip -c $raw_high | bedmap --delim '|' --multidelim '|' --echo --echo-map - old.temp  > raw_high.temp
+        cat raw_high.temp new.temp | sort-bed - > raw_high_updated.temp
+        gzip -c raw_high_updated.temp > $raw_high
+    fi
+  
+    gzip -c $high_pos.temp > $high_pos
+    rm -v $high_pos.temp
+    rm -v *.temp
+
+    # --- low-confidence --- #
+    if ! [ -a $raw_low ]
+    then
+        echo "Creating master list (low conf)!"
+        gzip -c $low_pos.temp > $raw_low
     fi
 
-    # Tidy up
-    gzip -c $pos.temp > $pos
-    echo "Deleting files."
+    # Checks if this data already exists in the master list (low)
+    if gunzip -c $raw_low | grep -q $datatype\_${datatime/min/}_
+    then
+        echo "Master list (low conf) already includes these regions - skipping."
+    else
+        # Adds regions to existing list (assume same if ANY overlap between sets)
+        echo "Updating master list."
+        gunzip -c $raw_low | bedmap --delim '|' --multidelim '|' --echo --indicator $low_pos.temp - > new_or_old.temp
+        awk 'BEGIN{FS="|"}{ if ($2==0) print $1 }' new_or_old.temp > new.temp
+        awk 'BEGIN{FS="|"}{ if ($2==1) print $1 }' new_or_old.temp > old.temp
+        gunzip -c $raw_low | bedmap --delim '|' --multidelim '|' --echo --echo-map - old.temp  > raw_low.temp
+        cat raw_low.temp new.temp | sort-bed - > raw_low_updated.temp
+        gzip -c raw_low_updated.temp > $raw_low
+    fi
+
+    gzip -c $low_pos.temp > $low_pos
+    rm -v $low_pos.temp
     rm -v *.temp
-    rm -v $pos.temp
+
+    # Tidy up
     rm -v $preds.temp
 done
 
 # Call on the other script to tidy this up!
-echo "Master file created, processing!"
-python process_master.py $results $datatype
+echo "Master files created, processing!"
+python process_master.py $raw_high $proc_high $datatype
+python process_master.py $raw_low $proc_low $datatype
 
 echo "Looking at overlap with genomic regions and ChIPseq data!"
-
 awk 'BEGIN{OFS="\t"}{ if ($6=="+") { print $1, $2, $2+1, $4, $6 } else { print $1, $3-1, $3, $4, $6 } }' $gene_list > gene_starts.temp
 
-# You know what, just give me the whole path.
-gunzip -c $regionlist | sed '/inflation/d' > pred.temp
 # (stripping the header line)
+gunzip -c $proc_high | sed '/inflation/d' > pred_high.temp
+gunzip -c $proc_low | sed '/inflation/d' > pred_low.temp
 
 # Find out where the hits are!
 echo "Dividing hits into regions."
-bedmap --fraction-ref 0.5 --indicator pred.temp $gene_start > gene_start.temp
-bedmap --fraction-ref 0.5 --delim ';' --multidelim ';' --echo-map-id pred.temp $gene_body | awk '{ if (NF==0) print 0; else print $0 }' > gb.temp
-bedmap --fraction-ref 0.5 --indicator pred.temp $non_gene > non_gene.temp
+bedmap --fraction-ref 0.5 --indicator pred_high.temp $gene_start > gene_start_high.temp
+bedmap --fraction-ref 0.5 --indicator pred_low.temp $gene_start > gene_start_low.temp
+bedmap --fraction-ref 0.5 --delim ';' --multidelim ';' --echo-map-id pred_high.temp $gene_body | awk '{ if (NF==0) print 0; else print $0 }' > gb_high.temp
+bedmap --fraction-ref 0.5 --delim ';' --multidelim ';' --echo-map-id pred_low.temp $gene_body | awk '{ if (NF==0) print 0; else print $0 }' > gb_low.temp
+bedmap --fraction-ref 0.5 --indicator pred_high.temp $non_gene > non_gene_high.temp
+bedmap --fraction-ref 0.5 --indicator pred_low.temp $non_gene > non_gene_low.temp
 
 # Get the distance to the nearest gene_start... (for all hits!) (modify the master)
 echo "Getting distance to closest gene start."
-bedtools closest -d -a pred.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-2), $NF, $(NF-1) }' > dist.pre.temp
-python post_bedtools_join_closest.py
-# (creates dist.temp)
+bedtools closest -d -a pred_high.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-2), $NF, $(NF-1) }' > dist_pre_high.temp
+python post_bedtools_join_closest.py dist_pre_high.temp dist_high.temp
+bedtools closest -d -a pred_low.temp -b gene_starts.temp | awk 'BEGIN{OFS="\t"}{ print $4, $(NF-2), $NF, $(NF-1) }' > dist_pre_low.temp
+python post_bedtools_join_closest.py dist_pre_low.temp dist_low.temp
+# (creates dist_high.temp and dist_low.temp)
 
 # Get histone counts! (totally unnormalised here...)
 echo "Getting histone counts."
-gunzip -c $H3K4me1 | bedmap --delim '\t' --range 500 --sum pred.temp - > H3K4me1.temp
-gunzip -c $H3K4me3 | bedmap --delim '\t' --range 500 --sum pred.temp - > H3K4me3.temp
-gunzip -c $H3K27ac | bedmap --delim '\t' --range 500 --sum pred.temp - > H3K27ac.temp
+gunzip -c $H3K4me1 | bedmap --delim '\t' --range 500 --sum pred_high.temp - > H3K4me1_high.temp
+gunzip -c $H3K4me3 | bedmap --delim '\t' --range 500 --sum pred_high.temp - > H3K4me3_high.temp
+gunzip -c $H3K27ac | bedmap --delim '\t' --range 500 --sum pred_high.temp - > H3K27ac_high.temp
+gunzip -c $H3K4me1 | bedmap --delim '\t' --range 500 --sum pred_low.temp - > H3K4me1_low.temp
+gunzip -c $H3K4me3 | bedmap --delim '\t' --range 500 --sum pred_low.temp - > H3K4me3_low.temp
+gunzip -c $H3K27ac | bedmap --delim '\t' --range 500 --sum pred_low.temp - > H3K27ac_low.temp
 
 echo "Combining!"
-paste pred.temp gene_start.temp gb.temp non_gene.temp dist.temp H3K4me1.temp H3K4me3.temp H3K27ac.temp > comb.temp
-gunzip -c $regionlist | head -1 | awk '{ print $0, "gene_start","gene_body","non_gene","closest_gene","distance","strand","H3K4me1","H3K4me3","H3K27ac" }' > headerfile.temp
-cat headerfile.temp comb.temp | gzip -c > $final
+# create a headerfile
+gunzip -c $proc_high | head -1 | awk '{ print $0, "gene_start","gene_body","non_gene","closest_gene","distance","strand","H3K4me1","H3K4me3","H3K27ac" }' > headerfile.temp
+# stick it all together
+paste pred_high.temp gene_start_high.temp gb_high.temp non_gene_high.temp dist_high.temp H3K4me1_high.temp H3K4me3_high.temp H3K27ac_high.temp > comb_high.temp
+cat headerfile.temp comb_high.temp | gzip -c > $pre_QC_confident
 
+paste pred_low.temp gene_start_low.temp gb_low.temp non_gene_low.temp dist_low.temp H3K4me1_low.temp H3K4me3_low.temp H3K27ac_low.temp > comb_low.temp
+cat headerfile.temp comb_low.temp | gzip -c > $maybe
+
+echo "Doing quality control!"
+R --file=QC.r --args $pre_QC_confident $confident
+
+# --- Create a log file of what just happened --- #
+echo `date` > $results/logfile.txt
+echo "Confident threshold:" $THRE_high >> $results/logfile.txt
+echo "Maybe threshold:" $THRE_low >> $results/logfile.txt
+echo "Datatype:" $datatype >> $results/logfile.txt
+echo "Suffix:" $suffix >> $results/logfile.txt
+echo "# High confidence regions:" `wc -l comb_high.temp | awk '{ print $1 }'` >> $results/logfile.txt
+echo "# Low confidence regions:" `wc -l comb_low.temp | awk '{ print $1 }'` >> $results/logfile.txt
+
+# --- Clean up ---- #
 echo "Tidying up!"
 rm -v *.temp
+rm -v $raw_high
+rm -v $raw_low
+rm -v $proc_high
+rm -v $proc_low
