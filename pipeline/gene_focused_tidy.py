364945292e4a9f8ef74a274bff13e222736433e6
Stephanie Hyland <steph.hyland@gmail.com>
2014-05-07 15:17:00 -0400

    takes stdin

--- pipeline/gene_focused_tidy.py
+++ pipeline/gene_focused_tidy.py
@@ -3,13 +3,8 @@
 
 import sys
 
-bedmap_mess=open('/Users/stephanie/ll/data/genes/analyse_FP/gene_list_with_overlaps.bed','r')
-bedmap_mess=open(sys.argv[1],'r')
-outfile=open(sys.argv[2],'w')
-#errorfile=open('/Users/stephanie/ll/data/genes/analyse_FP/inconsistent.txt','w')
-
 i=0
-for line in bedmap_mess:
+for line in sys.stdin:
     nGene_start=[]
     nBody=[]
     nOutside=[]
@@ -29,11 +24,5 @@ for line in bedmap_mess:
             if o.split()[13]!='0':
                 where_starts.append(o.split()[1])
                 where_ends.append(o.split()[2])
-    newline = genedata+'\t'+str(totalcounts)+'\t'+str(sum(nGene_start))+'\t'+str(sum(nBody))+'\t'+';'.join(which)+'\t'+';'.join(where_starts)+'\t'+';'.join(where_ends)
-    outfile.write(newline+'\n')
-#    if not sum(nGene_start) + sum(nBody) +sum(nOutside)== totalcounts:
-  #      print 'huh?', nGene_start, nBody, totalcounts, line
-#        print i
-#        i=i+1
-        #errorfile.write(newline+'\t'+'-'.join(map(str,nOutside))+'\n')
-#        sys.exit()
+    outline = genedata+'\t'+str(totalcounts)+'\t'+str(sum(nGene_start))+'\t'+str(sum(nBody))+'\t'+';'.join(which)+'\t'+';'.join(where_starts)+'\t'+';'.join(where_ends)
+    print outline
