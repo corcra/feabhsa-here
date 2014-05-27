e488095b9271acb40953d15b4590046e60339b4d
Stephanie Hyland <steph.hyland@gmail.com>
2014-05-26 22:42:08 -0400

    print -> cat

--- pipeline/fns_rate_analysis.r
+++ pipeline/fns_rate_analysis.r
@@ -1,15 +1,38 @@
-cat("rate_analysis_fns.r: Loading functions!\n")
+cat("fns_rate_analysis.r: Loading functions!\n")
 
-get_region_of_relevance<-function(from,to,genes,timepoint){
+zero_time<-function(data){
+    zero<-data
+    n<-nrow(data)
+    zero$transition<-rep(0,n)
+    return(zero)
+}
+
+get_region_of_relevance<-function(from,to,genes,timepoint,reg_start,reg_end){
+    if(is.na(from)){
+        from<-zero_time(to)
+    }
 # merge timepoints
+    if(!is.na(reg_start)){
+        # use a fixed region if a fixed region distance is provided
+        fixed_region<-TRUE
+    } else{
+        fixed_region<-FALSE
+    }
     both<-merge(from,to,by=1)
     strand_info<-genes[,c(4,6)]
     names(strand_info)<-c("name","strand")
     both<-merge(both,strand_info,by=1)
 
 # look at transitions
-    starts<-ifelse(both$strand=="+",floor(both$gene_start.x+both$transition.x),floor(both$gene_end.x-both$transition.y))
-    ends<-ifelse(both$strand=="+",ceiling(both$gene_start.x+both$transition.y),ceiling(both$gene_end.x-both$transition.x))
+    if(fixed_region){
+        cat("fns_rate_analysis.r: Using fixed region for calculating region of relevance!\n")
+        starts<-ifelse(both$strand=="+",floor(both$gene_start.x+reg_start),floor(both$gene_end.x-reg_end))
+        ends<-ifelse(both$strand=="+",ceiling(both$gene_start.x+reg_end),ceiling(both$gene_end.x-reg_start))   
+    } else{
+        cat("fns_rate_analysis.r: Using variable region for calculating region of relevance!\n")
+        starts<-ifelse(both$strand=="+",floor(both$gene_start.x+both$transition.x),floor(both$gene_end.x-both$transition.y))
+        ends<-ifelse(both$strand=="+",ceiling(both$gene_start.x+both$transition.y),ceiling(both$gene_end.x-both$transition.x))
+    }
     chr<-both$chr.x
     name<-both$name
     filler<-rep("NA",length(name))
@@ -49,11 +72,11 @@ consistent_over_replicates<-function(data_1,data_2,time){
         cons<-data.frame(name,chr,gene_start,gene_end,transition)
         return(cons)
     } else{
-#        pdf(paste0("transition_reproducibility_",time,".pdf"))
-#        hist(both$transition.x-both$transition.y,breaks=100,xlab="Difference in transition points between replicates (bp)",main=paste0("Reproducibility of transition points (",time," min)"))
-#        abline(v=sd,col="red")
-#        abline(v=-sd,col="red")
-#        graphics.off()
+        pdf(paste0("transition_reproducibility_",time,".pdf"))
+        hist(both$transition.x-both$transition.y,breaks=100,xlab="Difference in transition points between replicates (bp)",main=paste0("Reproducibility of transition points (",time," min)"))
+        abline(v=sd,col="red")
+        abline(v=-sd,col="red")
+        graphics.off()
         cons<-subset(both,abs(transition.x-transition.y)<2*sd)
         lost<-subset(both,abs(transition.x-transition.y)>=2*sd)
         name<-cons$name
@@ -67,16 +90,20 @@ consistent_over_replicates<-function(data_1,data_2,time){
     }
 }
 
+
 get_rate<-function(from,to,timediff){
-# note, the name field should be the second one...
+    if(is.na(from)){
+        from<-zero_time(to)
+    }
+    # note, the name field should be the second one...
     both<-merge(from,to,by=1)
     name<-both$name
     chr<-both$chr.x
     start<-both$gene_start.x
     end<-both$gene_end.x
     rate<-(both$transition.y-both$transition.x)/(timediff*1000)
-    name<-both$name
-    df<-data.frame(chr,start,end,name,rate)
+    distance<-rate*(timediff*1000)
+    df<-data.frame(chr,start,end,name,rate,distance)
     df_pos<-subset(df,rate>0)
     return(df_pos)
 }
@@ -162,4 +189,28 @@ add_covars<-function(data,n_gb,int1len,n_exon,CpG_gb,H3K79me2){
 
 rateplot<-function(data,title){
     ggplot(data,aes(x=n_gb,y=rate,group=n_gb))+geom_boxplot()+theme_bw()+xlab("# dREG hits")+ylab("rate (kb/min)")+ggtitle(title)
+}
+
+get_ggm<-function(raw_rate,covars,THRESH){
+    rate<-roz(raw_rate)
+    S<-cov(cbind(rate,covars))
+    n<-ncol(S)
+    edge_mat<-matrix(1,n,n)
+    rownames(edge_mat)<-c("rate",colnames(covars))
+    colnames(edge_mat)<-c("rate",colnames(covars))
+    amat<-adjMatrix(edge_mat)
+    cg<-fitConGraph(amat,S,n)
+    congraph<-(abs(cg$Shat)>THRESH)*1
+    adjgraph<-congraph-diag(1,n)
+    g<-graph.adjacency(adjgraph,add.colnames="name",mode="undirected")
+    return(list("cg"=cg$Shat,"adjgraph"=adjgraph,"g"=g))
+}
+
+get_covar_array<-function(data){
+    covar_array<-data[,c("n_gb","int1len","n_exon","CpG","H3K79me2")]
+    roz.covar_array<-data.frame(roz(data$n_gb),roz(data$int1len),roz(data$n_exon),roz(data$CpG),roz(data$H3K79me2))
+    covar_label<-c("n_gb","int1len","n_exon","CpG","H3K79me")
+    colnames(covar_array)<-covar_label
+    colnames(roz.covar_array)<-covar_label
+    return(roz.covar_array)
 }
